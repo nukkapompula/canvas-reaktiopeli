4e03c4caa140f0ede8d8a4bd4443d848bf1de0b0
Arto Eskelinen <arto.eske@gmail.com>
2023-09-11 10:16:18 +0300

    pelin aloitusfunktio

--- toiminnot.js
+++ toiminnot.js
@@ -1,1 +1,14 @@
-console.log("toimii!")
+function lataa(){
+    peliAlue.aloita();
+}
+
+var peliAlue = {
+    canvas : document.createElement("canvas"),
+    aloita : function(){
+        console.log("funktio toimii");
+        this.canvas.width = 320;
+        this.canvas.height = 240;
+        this.konteksti = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+}
