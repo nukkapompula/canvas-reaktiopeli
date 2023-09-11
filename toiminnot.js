295b66d01f58c9f83a0cd67b745a7f920ff01568
Arto Eskelinen <arto.eske@gmail.com>
2023-09-11 12:37:38 +0300

    funktioita yhä lisää

--- toiminnot.js
+++ toiminnot.js
@@ -14,6 +14,10 @@ var peliAlue = {
         this.canvas.height = 240;
         this.konteksti = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.ajastin = setInterval(paivitaPeliAlue, 20);
+    },
+    tyhjenna : function(){
+        this.konteksti.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
@@ -23,5 +27,15 @@ function esine(x, y, colour, radius){
     this.y = y;
     this.colour = colour;
     this.radius = radius;
+    this.paivita = function(){
+        kti = peliAlue.konteksti;
+        kti.fillStyle = colour;
+        kti.fillRect(this.x, this.y, this.radius, this.radius);
+    }
 }
 
+// objektien sijainnin, pisteiden ym päivittäminen
+function paivitaPeliAlue(){
+    peliAlue.tyhjenna();
+    pallo.paivita();
+}
