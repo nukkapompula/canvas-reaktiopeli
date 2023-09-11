95b3a52fa71851b00e03516e21a7a93c10526543
Arto Eskelinen <arto.eske@gmail.com>
2023-09-11 13:22:13 +0300

    esineen liikkuminen arvotaan

--- toiminnot.js
+++ toiminnot.js
@@ -1,9 +1,21 @@
 var pallo;
 
+/*
+pallon liikkumissuunnat:
+0 = ylös, 1 = yläoikea, 2 = oikea, 3 = alaoikea,
+4 = alas, 5 = alavasen, 6 = vasen, 7 = ylävasen
+*/
+
 // pelin käynnistyminen, objektin luominen
 function lataa(){
     peliAlue.aloita();
-    pallo = new esine(50, 50, "yellow", 20);
+    pallo = new esine(160, 150, "yellow", 20);
+    pallo.suunnanArpominen();
+    document.getElementsByTagName("canvas")[0].addEventListener("click", testi);
+}
+
+function testi(event){
+    console.log(event.clientX, event.clientY);
 }
 
 // pelialueeseen liittyvät toiminnot
@@ -27,10 +39,54 @@ function esine(x, y, colour, radius){
     this.y = y;
     this.colour = colour;
     this.radius = radius;
+    this.nopeus = 1;
     this.paivita = function(){
         kti = peliAlue.konteksti;
         kti.fillStyle = colour;
         kti.fillRect(this.x, this.y, this.radius, this.radius);
+    }
+    this.liiku = function(){
+        if(this.suunta == 0){
+            this.y -= this.nopeus;
+        } else if(this.suunta == 1){
+            this.x += this.nopeus;
+            this.y -= this.nopeus;
+        } else if(this.suunta == 2){
+            this.x += this.nopeus;
+        } else if(this.suunta == 3){
+            this.x += this.nopeus;
+            this.y += this.nopeus;
+        } else if(this.suunta == 4){
+            this.y += this.nopeus;
+        } else if(this.suunta == 5){
+            this.x -= this.nopeus;
+            this.y += this.nopeus;
+        } else if(this.suunta == 6){
+            this.x -= this.nopeus;
+        } else {
+            this.x -= this.nopeus;
+            this.y -= this.nopeus;
+        }
+    }
+    this.suunnanArpominen = function(){
+        let arpa = Math.round(Math.random()*7);
+        if(arpa == 0){
+            this.suunta = 0;
+        } else if(arpa == 1){
+            this.suunta = 1;
+        } else if(arpa == 2){
+            this.suunta = 2;
+        } else if(arpa == 3){
+            this.suunta = 3;
+        } else if(arpa == 4){
+            this.suunta = 4;
+        } else if(arpa == 5){
+            this.suunta = 5;
+        } else if(arpa == 6){
+            this.suunta = 6;
+        } else {
+            this.suunta = 7;
+        }
     }
 }
 
@@ -38,4 +94,5 @@ function esine(x, y, colour, radius){
 function paivitaPeliAlue(){
     peliAlue.tyhjenna();
     pallo.paivita();
+    pallo.liiku();
 }
