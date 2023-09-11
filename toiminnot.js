d61d9a8c0683ae45542e61c61a289346450116ed
Arto Eskelinen <arto.eske@gmail.com>
2023-09-11 11:00:25 +0300

    lisää funktioita

--- toiminnot.js
+++ toiminnot.js
@@ -1,14 +1,27 @@
+var pallo;
+
+// pelin käynnistyminen, objektin luominen
 function lataa(){
     peliAlue.aloita();
+    pallo = new esine(50, 50, "yellow", 20);
 }
 
+// pelialueeseen liittyvät toiminnot
 var peliAlue = {
     canvas : document.createElement("canvas"),
     aloita : function(){
-        console.log("funktio toimii");
         this.canvas.width = 320;
         this.canvas.height = 240;
         this.konteksti = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     }
 }
+
+// klikattavan objektin ominaisuudet, funktiot jne
+function esine(x, y, colour, radius){
+    this.x = x;
+    this.y = y;
+    this.colour = colour;
+    this.radius = radius;
+}
+
