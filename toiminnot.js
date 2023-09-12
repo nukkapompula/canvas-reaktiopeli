a13088a27dcd5168b4cbe9232c341ce7402525d5
Arto Eskelinen <arto.eske@gmail.com>
2023-09-12 08:06:06 +0300

    kommenttien siirto, pallon nopeus

--- toiminnot.js
+++ toiminnot.js
@@ -1,12 +1,6 @@
 var pallo;
 
-/*
-pallon liikkumissuunnat:
-0 = ylös, 1 = yläoikea, 2 = oikea, 3 = alaoikea,
-4 = alas, 5 = alavasen, 6 = vasen, 7 = ylävasen
-*/
-
-// pelin käynnistyminen, objektin luominen
+// pelin käynnistyminen, pallon luominen
 function lataa(){
     peliAlue.aloita();
     pallo = new esine("yellow", 20);
@@ -34,7 +28,7 @@ var peliAlue = {
     }
 }
 
-// klikattavan objektin ominaisuudet, funktiot jne
+// klikattavan pallon ominaisuudet, funktiot jne
 function esine(colour, radius){
     this.colour = colour;
     this.radius = radius;
@@ -42,32 +36,40 @@ function esine(colour, radius){
     this.paivita = function(){
         kti = peliAlue.konteksti;
         kti.fillStyle = colour;
-        kti.fillRect(this.x, this.y, this.radius, this.radius);
+        kti.beginPath();
+        kti.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        kti.fill();
+        // kti.fillRect(this.x, this.y, this.radius, this.radius);
     }
     this.liiku = function(){
         if(this.suunta == 0){
             this.y -= this.nopeus;
         } else if(this.suunta == 1){
-            this.x += this.nopeus;
-            this.y -= this.nopeus;
+            this.x += this.nopeus * 0.8;
+            this.y -= this.nopeus * 0.8;
         } else if(this.suunta == 2){
             this.x += this.nopeus;
         } else if(this.suunta == 3){
-            this.x += this.nopeus;
-            this.y += this.nopeus;
+            this.x += this.nopeus * 0.8;
+            this.y += this.nopeus * 0.8;
         } else if(this.suunta == 4){
             this.y += this.nopeus;
         } else if(this.suunta == 5){
-            this.x -= this.nopeus;
-            this.y += this.nopeus;
+            this.x -= this.nopeus * 0.8;
+            this.y += this.nopeus * 0.8;
         } else if(this.suunta == 6){
             this.x -= this.nopeus;
         } else {
-            this.x -= this.nopeus;
-            this.y -= this.nopeus;
+            this.x -= this.nopeus * 0.8;
+            this.y -= this.nopeus * 0.8;
         }
     }
     this.suunnanArpominen = function(){
+        /*
+        liikkumissuunnat:
+        0 = ylös, 1 = yläoikea, 2 = oikea, 3 = alaoikea,
+        4 = alas, 5 = alavasen, 6 = vasen, 7 = ylävasen
+        */
         let arpa = Math.round(Math.random()*7);
         if(arpa == 0){
             this.suunta = 0;
@@ -99,7 +101,7 @@ function esine(colour, radius){
     }
 }
 
-// objektien sijainnin, pisteiden ym päivittäminen
+// pallon sijainnin, pisteiden ym. päivittäminen
 function paivitaPeliAlue(){
     peliAlue.tyhjenna();
     pallo.paivita();
